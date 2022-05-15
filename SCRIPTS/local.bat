ff836c25296c603b5d35b19c94155333153f0e55
kryzbone <kryzbone@gmail.com>
2022-05-15 13:59:08 +0000

    update: update dev.bat and added local.bat

--- SCRIPTS/local.bat
+++ SCRIPTS/local.bat
@@ -1,6 +1,5 @@
 @echo off
 set DJANGO_READ_DOT_ENV_FILE=True
 set DATABASE_URL=postgres://{{USER}}:{{PASSWORD}}@localhost:{{PORT}}/{{DATABASE-NAME}}
-set DJANGO_SETTINGS_MODULE=config.settings.production
 
 ..\venv\scripts\activate
